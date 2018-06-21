61a2991c034b2d73a2cf5b57ac9a1cb5c42dc23c
JohnWhatson <40050875+JohnWhatson@users.noreply.github.com>
2018-06-21 18:35:08 -0500

    Add files via upload

---
+++ Proyecto Python.py
@@ -0,0 +1,44 @@
+#primero lanzar los dados y segun el numero que saque se elegirá al jugador x en la lista, después a este se le aplica el random para decidir si queda fuera o no
+
+from random import randint
+from time import sleep
+
+def Select(x,y):
+    if len(x)!=1:
+        if len(x)==y:
+            return x[y-1]
+        elif len(x)>y:
+            return x[y-1]
+        elif y>len(x):
+            while y>len(x):
+                y=y-len(x)
+            return x[y-1]
+    else:
+        return "Queda 1"
+
+def IfKill(x,y):
+    if Select(x,y)!="Queda 1":
+        k=randint(0,1)
+        if k==1:
+            a=Select(x,y)
+            x.remove(Select(x,y))
+            print(f"¡El/La jugador {a} ha sido eliminado/a!")
+            print("Suerte para la próxima ;)")
+            return "ELIMINADO/A"
+        else:
+            a = Select(x, y)
+            print(f"¡El/La jugador/a {a} se ha salvado!")
+            return "SALVADO/A"
+    else:
+        print("1 jugador/a restante...")
+        return "NO ELIMINAR"
+
+def Main(x,y):
+    while len(x)!=1:
+        print(f"Se ha seleccionado al jugador {Select(x,y)}")
+        print("UYUYUY....!")
+
+    return f"¡EL JUGADOR {x[0]} GANA!"
+
+
+Main(input().split(","),randint(1,6))
